fc326b9ac69e9a343fbebd4145f78507eac57861
sivasankar1999 <sivasankargannamani1999@gmail.com>
2020-02-10 22:57:42 +0530

    sample

---
+++ Calculator/src/main/java/Simple_Calculator/Calculator/Divison.java
@@ -0,0 +1,14 @@
+package Simple_Calculator.Calculator;
+
+public class Divison {
+	double x,y;
+	public Divison(double x,double y) {
+		// TODO Auto-generated constructor stub
+		this.x=x;
+		this.y=y;
+	}
+	void div() {
+		System.out.println(this.x/this.y);
+	}
+
+}
---
+++ Calculator/src/main/java/Simple_Calculator/Calculator/Substraction.java
@@ -0,0 +1,14 @@
+package Simple_Calculator.Calculator;
+
+public class Substraction {
+	double x,y;
+	public Substraction(double x,double y) {
+		// TODO Auto-generated constructor stub
+		this.x=x;
+		this.y=y;
+	}
+	void sub() {
+		System.out.println(this.x-this.y);
+	}
+
+}
